29e6461041a7f7955922b3848091d82e862c30c8
Oussama Elgoumri <euvoor@gmail.com>
2020-10-22 22:18:19 +0100

    wip

---
+++ database/migrations/3000_01_01_000000_modules.php
@@ -0,0 +1,38 @@
+<?php declare(strict_types=1);
+
+use Symfony\Component\Finder\Finder;
+use Illuminate\Database\Migrations\Migration;
+use Symfony\Component\Finder\Exception\DirectoryNotFoundException;
+
+class Modules extends Migration {
+    public function up() {
+        $finder = new Finder;
+
+        try {
+            $finder
+                ->files()
+                ->in(base_path("modules/*/database/migrations"))
+                ->name("/\.php$/")
+                ->sortByName(true);
+        }
+
+        catch (DirectoryNotFoundException $e) {
+            return;
+        }
+
+        foreach($finder as $file) {
+            $config = require $file->getPathInfo()->getPath() . "/../config/module.php";
+
+            if ($config["is_tenant"]) {
+                preg_match("/\/modules\/.*/", $file->getPathname(), $m);
+
+                Artisan::call("migrate", [
+                    "--path" => ltrim($m[0], "/"),
+                ]);
+
+                echo Artisan::output();
+            }
+        }
+    }
+}
+
