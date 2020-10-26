61add86e1929801ef47cf37a2a90bd06070c7085
Oussama Elgoumri <euvoor@gmail.com>
2020-10-26 21:56:24 +0100

    wip

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,7 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Arr;
 
 class User extends Authenticatable {
     use HasFactory, Notifiable, Traits\LocalizeDatetimeAttributes;
