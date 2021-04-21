0ed00158220cd4387df0ea4c73a26f764df2db86
Oussama Elgoumri <euvoor@gmail.com>
2021-04-21 03:14:23 +0000

    wip

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -8,6 +8,8 @@
         <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta name="csrf-token" content="{{ csrf_token() }}">
+        @stack("meta")
+
         <link rel="icon" type="image/x-icon" href="" />
         <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
 
