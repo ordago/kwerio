e56ee81d89761df343618532cde1b0018e5402b6
Oussama Elgoumri <euvoor@gmail.com>
2021-04-26 02:48:31 +0000

    wip

--- resources/js/pages/Users/Upsert/I18n.jsx
+++ resources/js/pages/Users/Upsert/I18n.jsx
@@ -1,12 +1,14 @@
-import { TextField } from "@material-ui/core"
+import { Grid, TextField, Typography } from "@material-ui/core"
 import { useDispatch, useSelector } from "react-redux"
 import Autocomplete, { createFilterOptions } from "@material-ui/lab/Autocomplete"
 import React from "react"
 
 import { actions } from "../index.slice"
+import useT from "../../../hooks/useT"
 
 function I18n() {
   const state = useSelector(state => state.users),
+    t = useT(),
     { locale, timezone, locale_iso_format } = state.upsert,
     dispatch = useDispatch()
 
@@ -14,94 +16,99 @@ function I18n() {
   let localeIsoFormats = state.localeIsoFormats.filter(format => format.label === locale_iso_format.value)
 
   return (
-    <>
-      {/* Locale */}
-      <Autocomplete
-        options={state.languages}
-        autoHighlight
-        value={language.length > 0 ? language[0] : null}
-        getOptionLabel={(option) => option.native_name}
-        filterOptions={createFilterOptions({
-          stringify: option => `${option.name} ${option.native_name}`,
-        })}
-        onChange={(e, value) => {
-          dispatch(actions.handleChange({
-            name: locale.name,
-            value: _.isNull(value) ? "" : value.locale,
-          }))
-        }}
-        renderOption={(option) => option.name}
-        renderInput={(params) => (
-          <TextField
-            {...params}
-            label="Choose a language"
-            helperText={locale.error ? locale.helper_text : ""}
-            error={locale.error}
-            inputProps={{
-              ...params.inputProps,
-              autoComplete: 'new-password',
-            }}
-          />
-        )}
-      />
+    <Grid container spacing={2}>
+      <Grid item sm={6} md={4}>
+        {/* Locale */}
+        <Autocomplete
+          options={state.languages}
+          autoHighlight
+          value={language.length > 0 ? language[0] : null}
+          getOptionLabel={(option) => option.native_name}
+          filterOptions={createFilterOptions({
+            stringify: option => `${option.name} ${option.native_name}`,
+          })}
+          onChange={(e, value) => {
+            dispatch(actions.handleChange({
+              name: locale.name,
+              value: _.isNull(value) ? "" : value.locale,
+            }))
+          }}
+          renderOption={(option) => option.name}
+          renderInput={(params) => (
+            <TextField
+              {...params}
+              label="Choose a language"
+              helperText={locale.error ? locale.helper_text : ""}
+              error={locale.error}
+              inputProps={{
+                ...params.inputProps,
+                autoComplete: 'new-password',
+              }}
+            />
+          )}
+        />
+      </Grid>
 
-      {/* Locale */}
-      <Autocomplete
-        options={state.timezones}
-        autoHighlight
-        value={timezone.value || null}
-        getOptionLabel={(option) => option}
-        renderOption={option => option}
-        onChange={(e, value) => {
-          dispatch(actions.handleChange({
-            name: timezone.name,
-            value,
-          }))
-        }}
-        renderInput={(params) => (
-          <TextField
-            {...params}
-            label="Choose a timezone"
-            helperText={timezone.error ? timezone.helper_text : ""}
-            error={timezone.error}
-            inputProps={{
-              ...params.inputProps,
-              autoComplete: 'new-password',
-            }}
-          />
-        )}
-      />
+      <Grid item sm={6} md={4}>
+        {/* Locale */}
+        <Autocomplete
+          options={state.timezones}
+          autoHighlight
+          value={timezone.value || null}
+          getOptionLabel={(option) => option}
+          renderOption={option => option}
+          onChange={(e, value) => {
+            dispatch(actions.handleChange({
+              name: timezone.name,
+              value,
+            }))
+          }}
+          renderInput={(params) => (
+            <TextField
+              {...params}
+              label="Choose a timezone"
+              helperText={timezone.error ? timezone.helper_text : ""}
+              error={timezone.error}
+              inputProps={{
+                ...params.inputProps,
+                autoComplete: 'new-password',
+              }}
+            />
+          )}
+        />
+      </Grid>
 
-      {/* Locale ISO Format */}
-      <Autocomplete
-        options={state.localeIsoFormats}
-        autoHighlight
-        value={localeIsoFormats.length > 0 ? localeIsoFormats[0] : null}
-        getOptionLabel={(option) => option.example}
-        renderOption={option => (
-          <>{option.label} ({option.example})</>
-        )}
-        onChange={(e, value) => {
-          dispatch(actions.handleChange({
-            name: locale_iso_format.name,
-            value: _.isNull(value) ? "" : value.label,
-          }))
-        }}
-        renderInput={(params) => (
-          <TextField
-            {...params}
-            label="Choose a format"
-            helperText={locale_iso_format.error ? locale_iso_format.helper_text : ""}
-            error={locale_iso_format.error}
-            inputProps={{
-              ...params.inputProps,
-              autoComplete: 'new-password',
-            }}
-          />
-        )}
-      />
-
-    </>
+      <Grid item sm={6} md={4}>
+        {/* Locale ISO Format */}
+        <Autocomplete
+          options={state.localeIsoFormats}
+          autoHighlight
+          value={localeIsoFormats.length > 0 ? localeIsoFormats[0] : null}
+          getOptionLabel={(option) => option.example}
+          renderOption={option => (
+            <>{option.label} ({option.example})</>
+          )}
+          onChange={(e, value) => {
+            dispatch(actions.handleChange({
+              name: locale_iso_format.name,
+              value: _.isNull(value) ? "" : value.label,
+            }))
+          }}
+          renderInput={(params) => (
+            <TextField
+              {...params}
+              label="Choose a format"
+              helperText={locale_iso_format.error ? locale_iso_format.helper_text : ""}
+              error={locale_iso_format.error}
+              inputProps={{
+                ...params.inputProps,
+                autoComplete: 'new-password',
+              }}
+            />
+          )}
+        />
+      </Grid>
+    </Grid>
   )
 }
 
--- resources/js/pages/Users/Upsert/PersonalInfo.jsx
+++ resources/js/pages/Users/Upsert/PersonalInfo.jsx
@@ -1,81 +1,87 @@
-import { Divider, TextField } from "@material-ui/core"
+import { Grid, TextField, Typography } from "@material-ui/core"
 import { useDispatch, useSelector } from "react-redux"
 import React from "react"
 
 import { actions } from "../index.slice"
+import useT from "../../../hooks/useT"
 
 function PersonalInfo() {
   const state = useSelector(state => state.users),
+    t = useT(),
     { email, first_name, last_name, password, password_confirmation } = state.upsert,
     dispatch = useDispatch()
 
   return (
-    <>
-      {/* Email */}
-      <TextField
-        name={email.name}
-        type="email"
-        label="Email"
-        fullWidth
-        value={email.value}
-        onChange={e => dispatch(actions.handleChange({ name: e.target.name, value: e.target.value }))}
-        onBlur={e => dispatch(actions.handleBlur({ name: e.target.name, value: e.target.value }))}
-        helperText={email.error ? email.helper_text : ""}
-        error={email.error}
-      />
+    <Grid container spacing={2}>
+      <Grid item sm={12}>
+        {/* Email */}
+        <TextField
+          name={email.name}
+          type="email"
+          label={t("Email")}
+          fullWidth
+          value={email.value}
+          onChange={e => dispatch(actions.handleChange({ name: e.target.name, value: e.target.value }))}
+          onBlur={e => dispatch(actions.handleBlur({ name: e.target.name, value: e.target.value }))}
+          helperText={email.error ? email.helper_text : ""}
+          error={email.error}
+        />
+      </Grid>
 
-      {/* Password */}
-      <TextField
-        name={password.name}
-        type="password"
-        label="Password"
-        fullWidth
-        value={password.value}
-        onChange={e => dispatch(actions.handleChange({ name: e.target.name, value: e.target.value }))}
-        onBlur={e => dispatch(actions.handleBlur({ name: e.target.name, value: e.target.value }))}
-        helperText={password.error ? password.helper_text : ""}
-        error={password.error}
-      />
+      <Grid item sm={6}>
+        {/* Password */}
+        <TextField
+          name={password.name}
+          type="password"
+          label={t("Password")}
+          fullWidth
+          value={password.value}
+          onChange={e => dispatch(actions.handleChange({ name: e.target.name, value: e.target.value }))}
+          onBlur={e => dispatch(actions.handleBlur({ name: e.target.name, value: e.target.value }))}
+          helperText={password.error ? password.helper_text : ""}
+          error={password.error}
+        />
 
-      {/* Password Confirmation */}
-      <TextField
-        name={password_confirmation.name}
-        type="password"
-        label="Confirm password"
-        fullWidth
-        value={password_confirmation.value}
-        onChange={e => dispatch(actions.handleChange({ name: e.target.name, value: e.target.value }))}
-        onBlur={e => dispatch(actions.handleBlur({ name: e.target.name, value: e.target.value }))}
-        helperText={password_confirmation.error ? password_confirmation.helper_text : ""}
-        error={password_confirmation.error}
-      />
+        {/* First name */}
+        <TextField
+          name={first_name.name}
+          label={t("First name")}
+          fullWidth
+          value={first_name.value}
+          onChange={e => dispatch(actions.handleChange({ name: e.target.name, value: e.target.value }))}
+          onBlur={e => dispatch(actions.handleBlur({ name: e.target.name, value: e.target.value }))}
+          helperText={first_name.error ? first_name.helper_text : ""}
+          error={first_name.error}
+        />
+      </Grid>
 
-      <Divider />
+      <Grid item sm={6}>
+        {/* Password Confirmation */}
+        <TextField
+          name={password_confirmation.name}
+          type="password"
+          label={t("Confirm password")}
+          fullWidth
+          value={password_confirmation.value}
+          onChange={e => dispatch(actions.handleChange({ name: e.target.name, value: e.target.value }))}
+          onBlur={e => dispatch(actions.handleBlur({ name: e.target.name, value: e.target.value }))}
+          helperText={password_confirmation.error ? password_confirmation.helper_text : ""}
+          error={password_confirmation.error}
+        />
 
-      {/* First name */}
-      <TextField
-        name={first_name.name}
-        label="First name"
-        fullWidth
-        value={first_name.value}
-        onChange={e => dispatch(actions.handleChange({ name: e.target.name, value: e.target.value }))}
-        onBlur={e => dispatch(actions.handleBlur({ name: e.target.name, value: e.target.value }))}
-        helperText={first_name.error ? first_name.helper_text : ""}
-        error={first_name.error}
-      />
-
-      {/* Last name */}
-      <TextField
-        name={last_name.name}
-        label="Last name"
-        fullWidth
-        value={last_name.value}
-        onChange={e => dispatch(actions.handleChange({ name: e.target.name, value: e.target.value }))}
-        onBlur={e => dispatch(actions.handleBlur({ name: e.target.name, value: e.target.value }))}
-        helperText={last_name.error ? last_name.helper_text : ""}
-        error={last_name.error}
-      />
-    </>
+        {/* Last name */}
+        <TextField
+          name={last_name.name}
+          label={t("Last name")}
+          fullWidth
+          value={last_name.value}
+          onChange={e => dispatch(actions.handleChange({ name: e.target.name, value: e.target.value }))}
+          onBlur={e => dispatch(actions.handleBlur({ name: e.target.name, value: e.target.value }))}
+          helperText={last_name.error ? last_name.helper_text : ""}
+          error={last_name.error}
+        />
+      </Grid>
+    </Grid>
   )
 }
 
