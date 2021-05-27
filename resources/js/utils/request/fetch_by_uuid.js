07d5dbfbc040af7fd362b5e576d612ec174a5349
Oussama Elgoumri <euvoor@gmail.com>
2021-05-27 19:32:54 +0100

    add hooks to fetch_by_uuid

--- resources/js/utils/request/fetch_by_uuid.js
+++ resources/js/utils/request/fetch_by_uuid.js
@@ -1,6 +1,14 @@
 import _ from "lodash"
 
-export default function({ actions, api, params }) {
+export default function({
+  actions,
+  api,
+  params,
+}, {
+  on200 = null,
+  before200 = null,
+  after200 = null,
+} = {}) {
   let endpoint = api.fetch_by_uuid
 
   for (let key in params.substitute) {
@@ -12,9 +20,23 @@ export default function({ actions, api, params }) {
     data: {
       uuid: params.uuid,
     },
-    200: ({ dispatch, data }) => {
+    200: args => {
+      if (on200) {
+        return on200(args)
+      }
+
+      if (before200) {
+        before200(args)
+      }
+
+      const { dispatch, data } = args
+
       dispatch(actions.upsertOne({ ...data.items[0] }))
       dispatch(actions.fillUpsert(data.items[0]))
+
+      if (after200) {
+        after200(args)
+      }
 
       return data
     }
