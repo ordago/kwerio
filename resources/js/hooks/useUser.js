7796a877545aec22a0e7d02e6010cec5dbe808cc
Oussama Elgoumri <euvoor@gmail.com>
2021-07-29 12:03:19 +0100

    rename module to moduleState on useUser hook

--- resources/js/hooks/useUser.js
+++ resources/js/hooks/useUser.js
@@ -2,23 +2,24 @@ import { useSelector } from "react-redux"
 
 export default function() {
   const user = useSelector(state => state.app.user),
-    module = useSelector(state => state.module)
+    moduleState = useSelector(state => state.module)
 
   function _can(ability) {
     if (user.owner_at !== null) {
       return true
     }
 
-    if (typeof module !== "undefined") {
-      ability = `${module.uid}/${ability}`
+    if (typeof moduleState !== "undefined") {
+      ability = `${moduleState.uid}/${ability}`
     }
 
     return user.abilities.indexOf(ability) !== -1
   }
 
   return {
-    can: (ability) => _can(ability),
-    canAny: (abilities) => {
+    can: ability => _can(ability),
+    cant: ability => ! _can(ability),
+    canAny: abilities => {
       for (let i = 0; i < abilities.length; i ++) {
         if (_can(abilities[i])) {
           return true
